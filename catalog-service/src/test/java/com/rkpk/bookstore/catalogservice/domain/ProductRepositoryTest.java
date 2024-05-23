7e73d5a0d58fa642d60e746ab90e95128bf88130
Rick Park <ricksmp95@gmail.com>
2024-05-23 14:43:16 +0100

    add sql script and missing autowire annotation

--- catalog-service/src/test/java/com/rkpk/bookstore/catalogservice/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/rkpk/bookstore/catalogservice/domain/ProductRepositoryTest.java
@@ -3,7 +3,9 @@ package com.rkpk.bookstore.catalogservice.domain;
 import com.rkpk.bookstore.catalogservice.domain.entity.ProductEntity;
 import com.rkpk.bookstore.catalogservice.domain.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -15,8 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
                 "spring.test.database.replace=none",
                 "spring.datasource.url=jdbc:tc:postgresql:16-alpine:///db",
         })
+@Sql("/test-data.sql")
 public class ProductRepositoryTest {
-
+    @Autowired
     private ProductRepository productRepository;
 
     // You don't need to test the methods provided by Spring Data JPA.
