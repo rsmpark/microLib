79a38d909821923dd2e3e4866c1b258568c8c6de
Rick Park <ricksmp95@gmail.com>
2024-05-23 14:40:29 +0100

    add repository unit test

--- catalog-service/src/main/java/com/rkpk/bookstore/catalogservice/domain/repository/ProductRepository.java
+++ catalog-service/src/main/java/com/rkpk/bookstore/catalogservice/domain/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.rkpk.bookstore.catalogservice.domain.repository;
 import com.rkpk.bookstore.catalogservice.domain.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    Optional<ProductEntity> findByCode(String p100);
 }
